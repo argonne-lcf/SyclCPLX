a05991e6cd1634d99363eda205a4b6b7adc07ff3
aidan.belton <aidan.belton@codeplay.com>
2022-08-11 15:49:29 +0100

    Add type checking to device code

--- tests/test_marray_complex_types.cpp
+++ tests/test_marray_complex_types.cpp
@@ -8,8 +8,8 @@ constexpr std::size_t marray_test_size = 10;
 #define TEST_MATH_OP_TYPE(op_name, op)                                         \
   template <typename T, std::size_t NumElements>                               \
   struct test##_##op_name##_##types {                                          \
-    bool operator()() {                                                        \
-                                                                               \
+    bool operator()(sycl::queue &Q) {                                          \
+      /* Check host code */                                                    \
       static_assert(                                                           \
           std::is_same_v<                                                      \
               sycl::marray<complex<T>, NumElements>,                           \
@@ -28,6 +28,31 @@ constexpr std::size_t marray_test_size = 10;
                          decltype(std::declval<sycl::marray<T, NumElements>>() \
                                       op std::declval<sycl::marray<            \
                                           complex<T>, NumElements>>())>);      \
+                                                                               \
+      /* Check device code */                                                  \
+      Q.single_task([=]() {                                                    \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<complex<T>, NumElements>,                         \
+                decltype(std::declval<sycl::marray<complex<T>, NumElements>>() \
+                             op std::declval<                                  \
+                                 sycl::marray<complex<T>, NumElements>>())>);  \
+                                                                               \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<complex<T>, NumElements>,                         \
+                decltype(                                                      \
+                    std::declval<sycl::marray<complex<T>, NumElements>>()      \
+                        op std::declval<sycl::marray<T, NumElements>>())>);    \
+                                                                               \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<complex<T>, NumElements>,                         \
+                decltype(std::declval<sycl::marray<T, NumElements>>()          \
+                             op std::declval<                                  \
+                                 sycl::marray<complex<T>, NumElements>>())>);  \
+      });                                                                      \
+                                                                               \
       return true;                                                             \
     }                                                                          \
   };
@@ -42,8 +67,8 @@ TEST_MATH_OP_TYPE(div, /)
 #define TEST_LOGIC_OP_TYPE(op_name, op)                                        \
   template <typename T, std::size_t NumElements>                               \
   struct test##_##op_name##_##types {                                          \
-    bool operator()() {                                                        \
-                                                                               \
+    bool operator()(sycl::queue &Q) {                                          \
+      /* Check host code */                                                    \
       static_assert(                                                           \
           std::is_same_v<                                                      \
               sycl::marray<bool, NumElements>,                                 \
@@ -63,6 +88,30 @@ TEST_MATH_OP_TYPE(div, /)
                                       op std::declval<sycl::marray<            \
                                           complex<T>, NumElements>>())>);      \
                                                                                \
+      /* Check device code */                                                  \
+      Q.single_task([=]() {                                                    \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<bool, NumElements>,                               \
+                decltype(std::declval<sycl::marray<complex<T>, NumElements>>() \
+                             op std::declval<                                  \
+                                 sycl::marray<complex<T>, NumElements>>())>);  \
+                                                                               \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<bool, NumElements>,                               \
+                decltype(                                                      \
+                    std::declval<sycl::marray<complex<T>, NumElements>>()      \
+                        op std::declval<sycl::marray<T, NumElements>>())>);    \
+                                                                               \
+        static_assert(                                                         \
+            std::is_same_v<                                                    \
+                sycl::marray<bool, NumElements>,                               \
+                decltype(std::declval<sycl::marray<T, NumElements>>()          \
+                             op std::declval<                                  \
+                                 sycl::marray<complex<T>, NumElements>>())>);  \
+      });                                                                      \
+                                                                               \
       return true;                                                             \
     }                                                                          \
   };
@@ -72,24 +121,33 @@ TEST_LOGIC_OP_TYPE(inv_eq, !=)
 #undef TEST_LOGIC_OP_TYPE
 
 // Check operations return correct types
-void check_math_operator_types() {
-  test_valid_types<test_add_types, marray_test_size>();
-  test_valid_types<test_sub_types, marray_test_size>();
-  test_valid_types<test_mul_types, marray_test_size>();
-  test_valid_types<test_div_types, marray_test_size>();
+void check_math_operator_types(sycl::queue &Q) {
+  test_valid_types<test_add_types, marray_test_size>(Q);
+  test_valid_types<test_sub_types, marray_test_size>(Q);
+  test_valid_types<test_mul_types, marray_test_size>(Q);
+  test_valid_types<test_div_types, marray_test_size>(Q);
 
-  test_valid_types<test_eq_types, marray_test_size>();
-  // test_valid_types<test_inv_eq_types, marray_test_size>();
+  test_valid_types<test_eq_types, marray_test_size>(Q);
+  test_valid_types<test_inv_eq_types, marray_test_size>(Q);
 }
 
 // Define math function tests
 #define TEST_MATH_FUNC_TYPE(func)                                              \
   template <typename T, std::size_t NumElements>                               \
   struct test##_##func##_##types {                                             \
-    bool operator()() {                                                        \
+    bool operator()(sycl::queue &Q) {                                          \
+      /* Check host code */                                                    \
       static_assert(std::is_same_v<                                            \
                     sycl::marray<complex<T>, NumElements>,                     \
                     decltype(func(sycl::marray<complex<T>, NumElements>()))>); \
+                                                                               \
+      /* Check device code */                                                  \
+      Q.single_task([=]() {                                                    \
+        static_assert(                                                         \
+            std::is_same_v<sycl::marray<complex<T>, NumElements>,              \
+                           decltype(func(                                      \
+                               sycl::marray<complex<T>, NumElements>()))>);    \
+      });                                                                      \
       return true;                                                             \
     }                                                                          \
   };
@@ -115,16 +173,26 @@ TEST_MATH_FUNC_TYPE(tanh)
 #undef TEST_MATH_FUNC_TYPE
 
 template <typename T, std::size_t NumElements> struct test_abs_types {
-  bool operator()() {
+  bool operator()(sycl::queue &Q) {
+    /* Check host code */
     static_assert(
         std::is_same_v<sycl::marray<T, NumElements>,
                        decltype(abs(sycl::marray<complex<T>, NumElements>()))>);
+
+    /* Check device code */
+    Q.single_task([=]() {
+      static_assert(std::is_same_v<
+                    sycl::marray<T, NumElements>,
+                    decltype(abs(sycl::marray<complex<T>, NumElements>()))>);
+    });
+
     return true;
   }
 };
 
 template <typename T, std::size_t NumElements> struct test_polar_types {
-  bool operator()() {
+  bool operator()(sycl::queue &Q) {
+    /* Check host code */
     static_assert(
         std::is_same_v<sycl::marray<complex<T>, NumElements>,
                        decltype(polar(sycl::marray<T, NumElements>()))>);
@@ -138,12 +206,31 @@ template <typename T, std::size_t NumElements> struct test_polar_types {
     static_assert(
         std::is_same_v<sycl::marray<complex<T>, NumElements>,
                        decltype(polar(T(), sycl::marray<T, NumElements>()))>);
+
+    /* Check device code */
+    Q.single_task([=]() {
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(polar(sycl::marray<T, NumElements>()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(polar(sycl::marray<T, NumElements>(),
+                                        sycl::marray<T, NumElements>()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(polar(sycl::marray<T, NumElements>(), T()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(polar(T(), sycl::marray<T, NumElements>()))>);
+    });
+
     return true;
   }
 };
 
 template <typename T, std::size_t NumElements> struct test_pow_types {
-  bool operator()() {
+  bool operator()(sycl::queue &Q) {
+    /* Check host code */
     // complex-deci
     static_assert(
         std::is_same_v<sycl::marray<complex<T>, NumElements>,
@@ -181,39 +268,86 @@ template <typename T, std::size_t NumElements> struct test_pow_types {
     static_assert(std::is_same_v<
                   sycl::marray<complex<T>, NumElements>,
                   decltype(pow(T(), sycl::marray<complex<T>, NumElements>()))>);
+
+    /* Check device code */
+    Q.single_task([=]() {
+      // complex-deci
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(pow(sycl::marray<complex<T>, NumElements>(),
+                                      sycl::marray<T, NumElements>()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(pow(sycl::marray<complex<T>, NumElements>(),
+                                      T()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(pow(complex<T>(),
+                                      sycl::marray<T, NumElements>()))>);
+
+      // complex-complex
+      static_assert(std::is_same_v<
+                    sycl::marray<complex<T>, NumElements>,
+                    decltype(pow(sycl::marray<complex<T>, NumElements>(),
+                                 sycl::marray<complex<T>, NumElements>()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(pow(sycl::marray<complex<T>, NumElements>(),
+                                      complex<T>()))>);
+      static_assert(std::is_same_v<
+                    sycl::marray<complex<T>, NumElements>,
+                    decltype(pow(complex<T>(),
+                                 sycl::marray<complex<T>, NumElements>()))>);
+
+      // deci-complx
+      static_assert(std::is_same_v<
+                    sycl::marray<complex<T>, NumElements>,
+                    decltype(pow(sycl::marray<T, NumElements>(),
+                                 sycl::marray<complex<T>, NumElements>()))>);
+      static_assert(std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                                   decltype(pow(sycl::marray<T, NumElements>(),
+                                                complex<T>()))>);
+      static_assert(
+          std::is_same_v<sycl::marray<complex<T>, NumElements>,
+                         decltype(pow(
+                             T(), sycl::marray<complex<T>, NumElements>()))>);
+    });
+
     return true;
   }
 };
 
 // Check functions return correct types
-void check_math_function_types() {
+void check_math_function_types(sycl::queue &Q) {
 
-  test_valid_types<test_abs_types, marray_test_size>();
-  test_valid_types<test_acos_types, marray_test_size>();
-  test_valid_types<test_asin_types, marray_test_size>();
-  test_valid_types<test_atan_types, marray_test_size>();
-  test_valid_types<test_acosh_types, marray_test_size>();
-  test_valid_types<test_asinh_types, marray_test_size>();
-  test_valid_types<test_atanh_types, marray_test_size>();
-  test_valid_types<test_conj_types, marray_test_size>();
-  test_valid_types<test_cos_types, marray_test_size>();
-  test_valid_types<test_cosh_types, marray_test_size>();
-  test_valid_types<test_exp_types, marray_test_size>();
-  test_valid_types<test_log_types, marray_test_size>();
-  test_valid_types<test_log10_types, marray_test_size>();
-  test_valid_types<test_polar_types, marray_test_size>();
-  test_valid_types<test_pow_types, marray_test_size>();
-  test_valid_types<test_proj_types, marray_test_size>();
-  test_valid_types<test_sin_types, marray_test_size>();
-  test_valid_types<test_sinh_types, marray_test_size>();
-  test_valid_types<test_sqrt_types, marray_test_size>();
-  test_valid_types<test_tan_types, marray_test_size>();
-  test_valid_types<test_tanh_types, marray_test_size>();
+  test_valid_types<test_abs_types, marray_test_size>(Q);
+  test_valid_types<test_acos_types, marray_test_size>(Q);
+  test_valid_types<test_asin_types, marray_test_size>(Q);
+  test_valid_types<test_atan_types, marray_test_size>(Q);
+  test_valid_types<test_acosh_types, marray_test_size>(Q);
+  test_valid_types<test_asinh_types, marray_test_size>(Q);
+  test_valid_types<test_atanh_types, marray_test_size>(Q);
+  test_valid_types<test_conj_types, marray_test_size>(Q);
+  test_valid_types<test_cos_types, marray_test_size>(Q);
+  test_valid_types<test_cosh_types, marray_test_size>(Q);
+  test_valid_types<test_exp_types, marray_test_size>(Q);
+  test_valid_types<test_log_types, marray_test_size>(Q);
+  test_valid_types<test_log10_types, marray_test_size>(Q);
+  test_valid_types<test_polar_types, marray_test_size>(Q);
+  test_valid_types<test_pow_types, marray_test_size>(Q);
+  test_valid_types<test_proj_types, marray_test_size>(Q);
+  test_valid_types<test_sin_types, marray_test_size>(Q);
+  test_valid_types<test_sinh_types, marray_test_size>(Q);
+  test_valid_types<test_sqrt_types, marray_test_size>(Q);
+  test_valid_types<test_tan_types, marray_test_size>(Q);
+  test_valid_types<test_tanh_types, marray_test_size>(Q);
 }
 
 int main() {
-  check_math_function_types();
-  check_math_operator_types();
+  sycl::queue Q;
+
+  check_math_function_types(Q);
+  check_math_operator_types(Q);
 
   return 0;
 }
