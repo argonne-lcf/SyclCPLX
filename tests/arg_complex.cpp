86ac78de5756860ae559549570d3737715f0f1a2
jle-quel <jeffersonlequellec@me.com>
2022-12-12 11:14:20 +0100

    Update arg tests to also test the pair(T) function

--- tests/arg_complex.cpp
+++ tests/arg_complex.cpp
@@ -1,6 +1,6 @@
 #include "test_helper.hpp"
 
-TEMPLATE_TEST_CASE("Test complex arg", "[arg]", double, float, sycl::half) {
+TEMPLATE_TEST_CASE("Test complex arg cmplx", "[arg]", double, float, sycl::half) {
   using T = TestType;
 
   sycl::queue Q;
@@ -33,6 +33,48 @@ TEMPLATE_TEST_CASE("Test complex arg", "[arg]", double, float, sycl::half) {
 
   // Check cplx::complex output from host
   cplx_out[0] = sycl::ext::cplx::arg<T>(cplx_input);
+
+  check_results(cplx_out[0], std_out);
+
+  sycl::free(cplx_out, Q);
+}
+
+TEMPLATE_TEST_CASE("Test complex arg deci", "[arg]",
+(std::pair<double, bool>),
+(std::pair<double, char>),
+(std::pair<double, int>),
+(std::pair<sycl::half, sycl::half>),
+(std::pair<float, float>),
+(std::pair<double, double>)) {
+
+  using T = typename TestType::first_type;
+  using X = typename TestType::second_type;
+
+  sycl::queue Q;
+
+  // Test cases
+  // Note: Output is undefined if val is Nan
+  X input = GENERATE(4.42, 2.02, inf_val<T>);
+
+  auto std_in = init_deci(input);
+
+  T std_out{};
+  auto *cplx_out = sycl::malloc_shared<T>(1, Q);
+
+  // Get std::complex output
+  std_out = std::arg(std_in);
+
+  // Check cplx::complex output from device
+  if (is_type_supported<X>(Q)) {
+    Q.single_task([=]() {
+       cplx_out[0] = sycl::ext::cplx::arg<X>(std_in);
+     }).wait();
+
+    check_results(cplx_out[0], std_out);
+  }
+
+  // Check cplx::complex output from host
+  cplx_out[0] = sycl::ext::cplx::arg<X>(std_in);
 
   check_results(cplx_out[0], std_out);
 
