ae78048c2082b3665cb2574544e892f7c5e2ffd3
jle-quel <jeffersonlequellec@me.com>
2022-12-12 11:16:09 +0100

    Update pow tests to also test the pow(cplx<T>, cplx<U>) function

--- tests/pow_complex.cpp
+++ tests/pow_complex.cpp
@@ -50,6 +50,60 @@ TEMPLATE_TEST_CASE("Test complex pow cplx-cplx overload", "[pow]", double,
   sycl::free(cplx_out, Q);
 }
 
+TEMPLATE_TEST_CASE("Test complex pow cplx<T>-cplx<U> overload", "[pow]",
+(std::pair<double, float>),
+(std::pair<float, sycl::half>),
+(std::pair<sycl::half, double>)) {
+
+  using T = typename TestType::first_type;
+  using X = typename TestType::second_type;
+
+  sycl::queue Q;
+
+  // Test cases
+  // Values are generated as cross product of input1 and input2's GENERATE list
+  cmplx<T> input1 = GENERATE(
+      cmplx<T>{4.42, 2.02}, cmplx<T>{inf_val<T>, 2.02},
+      cmplx<T>{4.42, inf_val<T>}, cmplx<T>{inf_val<T>, inf_val<T>},
+      cmplx<T>{nan_val<T>, 2.02}, cmplx<T>{4.42, nan_val<T>},
+      cmplx<T>{nan_val<T>, nan_val<T>}, cmplx<T>{nan_val<T>, inf_val<T>},
+      cmplx<T>{inf_val<T>, nan_val<T>});
+
+  cmplx<X> input2 = GENERATE(
+      cmplx<X>{4.42, 2.02}, cmplx<X>{inf_val<X>, 2.02},
+      cmplx<X>{4.42, inf_val<X>}, cmplx<X>{inf_val<X>, inf_val<X>},
+      cmplx<X>{nan_val<X>, 2.02}, cmplx<X>{4.42, nan_val<X>},
+      cmplx<X>{nan_val<X>, nan_val<X>}, cmplx<X>{nan_val<X>, inf_val<X>},
+      cmplx<X>{inf_val<X>, nan_val<X>});
+
+  auto std_in1 = init_std_complex(input1);
+  auto std_in2 = init_std_complex(input2);
+  sycl::ext::cplx::complex<T> cplx_input1{input1.re, input1.im};
+  sycl::ext::cplx::complex<X> cplx_input2{input2.re, input2.im};
+
+  std::complex<T> std_out{};
+  auto *cplx_out = sycl::malloc_shared<sycl::ext::cplx::complex<T>>(1, Q);
+
+  // Get std::complex output
+  std_out = std::pow(std_in1, std_in2);
+
+  // Check cplx::complex output from device
+  if (is_type_supported<T>(Q)) {
+    Q.single_task([=]() {
+       cplx_out[0] = sycl::ext::cplx::pow<T>(cplx_input1, cplx_input2);
+     }).wait();
+
+    check_results(cplx_out[0], std_out);
+  }
+
+  // Check cplx::complex output from host
+  cplx_out[0] = sycl::ext::cplx::pow<T>(cplx_input1, cplx_input2);
+
+  check_results(cplx_out[0], std_out);
+
+  sycl::free(cplx_out, Q);
+}
+
 TEMPLATE_TEST_CASE("Test complex pow cplx-deci overload", "[pow]", double,
                    float, sycl::half) {
   using T = TestType;
