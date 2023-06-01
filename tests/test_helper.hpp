dd4f27c2c59076c48344593b68ab4d085968c07a
jle-quel <jeffersonlequellec@me.com>
2023-06-01 09:23:21 +0200

    improve test_helper for complex's group algorithms

--- tests/test_helper.hpp
+++ tests/test_helper.hpp
@@ -1,8 +1,8 @@
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
-#include <cmath>
-#include <iomanip>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
 #include <sycl/sycl.hpp>
 
 #include "sycl_ext_complex.hpp"
@@ -67,6 +67,8 @@ template <> struct numeric_limits<sycl::half> {
 } // namespace std
 #endif
 
+namespace detail {
+
 template <typename T, typename U> inline bool is_nan_or_inf(T x, U y) {
   return (std::isnan(x.real()) && std::isnan(y.real())) ||
          (std::isnan(x.imag()) && std::isnan(y.imag())) ||
@@ -74,7 +76,6 @@ template <typename T, typename U> inline bool is_nan_or_inf(T x, U y) {
          (std::isinf(x.imag()) && std::isinf(y.imag()));
 }
 
-namespace detail {
 template <typename T> bool almost_equal(T x, T y, int ulp) {
   if (std::isnan(x) && std::isnan(y))
     return true;
@@ -87,11 +88,12 @@ template <typename T> bool almost_equal(T x, T y, int ulp) {
 }
 
 template <typename T>
-bool almost_equal(sycl::ext::cplx::complex<T> x, std::complex<T> y, int ulp) {
-  auto diff = std::abs((std::complex<T>)x - y);
+bool almost_equal(std::complex<T> output, std::complex<T> reference, int ulp) {
+  auto diff = std::abs(output - reference);
   return diff <= std::numeric_limits<T>::epsilon() *
-                     std::abs((std::complex<T>)x + y) * ulp ||
-         diff < std::numeric_limits<T>::min() || is_nan_or_inf(x, y);
+                     std::abs(output + reference) * ulp ||
+         diff < std::numeric_limits<T>::min() ||
+         is_nan_or_inf(output, reference);
 }
 
 // Helpers for testing half
@@ -106,6 +108,7 @@ inline std::complex<float> trunc_float(std::complex<float> c) {
   auto c_sycl_half = static_cast<sycl::ext::cplx::complex<sycl::half>>(c);
   return sycl_half_to_float(c_sycl_half);
 }
+
 } // namespace detail
 
 // Helper for initializing std::complex values for tests only needed because
@@ -164,34 +167,68 @@ auto constexpr convert_marray(sycl::marray<std::complex<Tin>, NumElements> c) {
 
 // Helpers for comparing SyclCPLX and standard c++ results
 
+namespace detail {
+
 template <typename T>
-void check_results(sycl::ext::cplx::complex<T> output,
-                   std::complex<T> reference, int tol_multiplier = 1) {
+struct is_a_complex
+    : std::integral_constant<bool,
+                             sycl::ext::cplx::is_gencomplex_v<T> ||
+                                 std::is_same_v<std::complex<sycl::half>, T> ||
+                                 std::is_same_v<std::complex<float>, T> ||
+                                 std::is_same_v<std::complex<double>, T>> {};
+template <typename T>
+inline constexpr bool is_a_complex_v = is_a_complex<T>::value;
+
+} // namespace detail
+
+/// Specialization for double, float, sycl::half
+template <typename T>
+typename std::enable_if_t<sycl::ext::cplx::is_genfloat_v<T>, void>
+check_results(T output, T reference, int tol_multiplier = 1) {
   CHECK(detail::almost_equal(output, reference,
                              tol_multiplier * SYCL_CPLX_TOL_ULP));
 }
 
-template <typename T>
-void check_results(T output, T reference, int tol_multiplier = 1) {
-  CHECK(detail::almost_equal(output, reference,
+/// Specialization for sycl::complex and std::complex
+template <typename LHS, typename RHS>
+typename std::enable_if_t<
+    detail::is_a_complex_v<LHS> && detail::is_a_complex_v<RHS>, void>
+check_results(LHS output, RHS reference, int tol_multiplier = 1) {
+  using T1 = typename LHS::value_type;
+  using T2 = typename RHS::value_type;
+
+  if (!std::is_same_v<T1, T2>) {
+    FAIL("check_results can be called with sycl::complex and/or std::complex "
+         "but with the same value_type");
+  }
+
+  CHECK(detail::almost_equal(static_cast<std::complex<T1>>(output),
+                             static_cast<std::complex<T2>>(reference),
                              tol_multiplier * SYCL_CPLX_TOL_ULP));
 }
 
-template <typename T, std::size_t NumElements>
-void check_results(
-    sycl::marray<sycl::ext::cplx::complex<T>, NumElements> output,
-    sycl::marray<std::complex<T>, NumElements> reference,
-    int tol_multiplier = 1) {
-  for (std::size_t i = 0; i < NumElements; ++i) {
+/// Specialization for sycl::marray
+template <typename LHS, std::size_t NumElementsLHS, typename RHS,
+          std::size_t NumElementsRHS>
+void check_results(sycl::marray<LHS, NumElementsLHS> output,
+                   sycl::marray<RHS, NumElementsRHS> reference,
+                   int tol_multiplier = 1) {
+  if (NumElementsLHS != NumElementsRHS) {
+    FAIL("check_results can be called with sycl::marray but with the same "
+         "NumElement");
+  }
+
+  for (std::size_t i = 0; i < NumElementsLHS; ++i) {
     check_results(output[i], reference[i], tol_multiplier);
   }
 }
 
+/// Specialization for std::array
 template <typename T, std::size_t NumElements>
-void check_results(sycl::marray<T, NumElements> output,
-                   sycl::marray<T, NumElements> reference,
+void check_results(std::array<T, NumElements> output,
+                   std::array<T, NumElements> reference,
                    int tol_multiplier = 1) {
   for (std::size_t i = 0; i < NumElements; ++i) {
-    check_results<T>(output[i], reference[i], tol_multiplier);
+    check_results(output[i], reference[i], tol_multiplier);
   }
 }
